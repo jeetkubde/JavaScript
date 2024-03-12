4899816f6cf4c0eb6c313759f53a4f6a9822f8b0
jeet kubde <114217260+jeetkubde@users.noreply.github.com>
2024-03-12 19:19:29 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12365563
+let accountEmail = "jeet@gmail.com"
+var acccountPassword = "123654"
+accountCity = "amt"
+
+
+// accountId = 2
+accountEmail = "jk@gmail.com"
+acccountPassword = "akola"
+accountCity ="mumbai"
+
+console.table([accountId, accountEmail,  acccountPassword, accountCity])
+
+console.log(accountId);
