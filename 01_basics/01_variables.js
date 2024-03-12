277a21a01cac5950f301b941732a8410e5864a4d
jeet kubde <114217260+jeetkubde@users.noreply.github.com>
2024-03-12 19:27:33 +0000

    container config

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,13 +2,17 @@ const accountId = 12365563
 let accountEmail = "jeet@gmail.com"
 var acccountPassword = "123654"
 accountCity = "amt"
+let accountState;
 
 
-// accountId = 2
+// accountId = 2 not allowed 
 accountEmail = "jk@gmail.com"
 acccountPassword = "akola"
 accountCity ="mumbai"
 
-console.table([accountId, accountEmail,  acccountPassword, accountCity])
-
 console.log(accountId);
+
+/* prefer not to use var because  of issue in block scope andfunctional scope */
+
+console.table([accountId, accountEmail,  acccountPassword, accountCity,accountState])
+
