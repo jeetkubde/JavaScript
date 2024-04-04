2448fb34302341b34abd1ad6f056e34ce6d2e54d
jeet kubde <114217260+jeetkubde@users.noreply.github.com>
2024-04-04 12:35:18 +0000

    ontainer config

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+let score = "33abc"//null//undefined 
 
 console.log(typeof score);
 console.log(typeof (score));
@@ -6,7 +6,6 @@ console.log(typeof (score));
 let valueInNumber = Number(score)
 
 console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
+console.log(valueInNumber);  
 
 //////////////////////////////
