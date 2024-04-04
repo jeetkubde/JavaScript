bd58600c2a466fa695e73f2b406d028c7b3154ec
jeet kubde <114217260+jeetkubde@users.noreply.github.com>
2024-04-04 12:57:01 +0000

    container config

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,33 @@
 let score = "33abc"//null//undefined 
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);  
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);  
 
+//"33" => 33
+//"33abc" => NaN
+// true => 1;  false => 0
 //////////////////////////////
+
+let isLoggedIn = "jeet" //"" //1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(typeof booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false 
+// "" => false 
+// "jeet" => true
+////////////////////////////////////
+
+let SomeNumber = 33
+
+let StringNumber = String(SomeNumber)
+
+console.log(typeof StringNumber)
+console.log(StringNumber);
+
