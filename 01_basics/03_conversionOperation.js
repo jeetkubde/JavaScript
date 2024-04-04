45c49b6c0f6b85d97bc5f49628e3db6eedae1fdc
jeet kubde <114217260+jeetkubde@users.noreply.github.com>
2024-04-04 13:40:57 +0000

    container config

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -53,9 +53,13 @@ let str3 = str1+str2
 
 ///////////////////////
 
-console.log("1" + 2);
+/*console.log("1" + 2);
 console.log(1 + "2");
 console.log("1" + 2 + 3);
 console.log(1 + 2 + "3");
-
 console.log(3 + 4 * 5 % 6);
+*/
+
+
+console.log(+true);
+console.log(+"");
